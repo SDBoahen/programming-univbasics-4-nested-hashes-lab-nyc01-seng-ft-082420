95900e38da26ea9a8838c892f36febcda27e373c
Darkwah Boahen (Sam) <sdboahen@gmail.com>
2020-08-17 02:47:30 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -94,7 +94,13 @@ def adding_matz
 
 
 	programmer_hash = 
- 		{
+		{
+		  
+		  :yukihiro_matsumoto => {
+      :known_for => "Ruby",
+      :languages => ["LISP", "C"]
+      },
+ 		  
       :grace_hopper => {
         :known_for => "COBOL",
         :languages => ["COBOL", "FORTRAN"]
@@ -110,9 +116,10 @@ def adding_matz
     }
   
     
-    programmer_hash[:yukihiro_matsumoto] = { }
+    # # x-x
+    # programmer_hash[:yukihiro_matsumoto] = { }
     
-    programmer_hash[:yukihiro_matsumoto] << { :known_for => "Ruby" , :languages => ["LISP", "C"] }
+    # programmer_hash[:yukihiro_matsumoto] << { :known_for => "Ruby" , :languages => ["LISP", "C"] }
       
       
     programmer_hash
